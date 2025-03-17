7120aabdd922a91dcb37dc5cadb79af7b97b20bf
Aydin Zahedivash <35406120+aydinzahedi@users.noreply.github.com>
2025-03-17 14:25:07 -0700

    No PHI warnings
    Larger text for PHI warning, and added to chat window

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ export default function AgentsPage() {
   const InfoCard = (
     <GuideInfoBox>
       <ul>
-        <li className="hidden text-l md:block">
+        <li className="hidden text-2xl md:block">
           👋
-          <span className="ml-2">
+          <span className="ml-2 text-xl">
             Welcome! This is a tool to help staff at Gardner Packard Children&apos;s Health Center navigate patient care resources. All information is sourced from {" "}
             <a href="https://med.stanford.edu/ppc.html" target="_blank">
               ppc.stanford.edu
@@ -18,11 +18,11 @@ export default function AgentsPage() {
       </ul>
 
       <ul>
-        <li className="hidden text-l md:block">
+        <li className="hidden text-2xl md:block">
           🛑
-          <span className="ml-2">
-            Please DO NOT include PHI in any of your searches.
-          </span>
+            <span className="ml-2 text-xl">
+            Please <u><strong>DO NOT include PHI</strong></u> in any of your searches.
+            </span>
         </li>
       </ul>
 
@@ -34,7 +34,7 @@ export default function AgentsPage() {
       emptyStateComponent={InfoCard}
       showIngestForm={false}
       placeholder={
-        'Enter your question here!'
+        'Enter your question here! (Do NOT include PHI)'
       }
       emoji="💡"
     />
